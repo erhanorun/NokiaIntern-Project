20df7c3f44879cebf706f07ee27b47c00d7c8418
erhanorun <52308384+erhanorun@users.noreply.github.com>
2021-09-13 21:54:55 +0300

    Add files via upload

---
+++ NokiaIntern/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Generate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Generate
+            // 
+            resources.ApplyResources(this.Generate, "Generate");
+            this.Generate.Name = "Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // dateTimePicker1
+            // 
+            resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Generate);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ NokiaIntern/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Xceed.Words.NET;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace WindowsFormsApp6
+
+
+{
+
+    public partial class Form1 : Form
+    {
+
+
+
+
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static string CreateMD5(string input)
+        {
+
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            word.Visible = true; 
+            Microsoft.Office.Interop.Word.Document wordDocument; 
+            object wordObj = System.Reflection.Missing.Value;
+            wordDocument = word.Documents.Add(ref wordObj);
+            word.Selection.TypeText(label13.Text + " " + label7.Text);
+            word.Selection.Font.Size = 24;
+            word.Selection.Font.Name = "Arial";
+            word = null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog2_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void button1_Click_1(object sender, EventArgs e)
+        {   
+            OpenFileDialog f1 = new OpenFileDialog();
+
+            string directory;
+            string folder;
+            
+             if (f1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folder = Path.GetFileName(f1.FileName);
+                directory = Path.GetDirectoryName(f1.FileName);
+                
+                
+                System.IO.FileInfo ff = new System.IO.FileInfo(f1.FileName);
+                string DosyaUzantisi = ff.Extension;
+                if (DosyaUzantisi == ".dll" ) {
+
+                    string[] filePaths = Directory.GetFiles(@directory, "*.docx");
+                    string wordfile = string.Join("", filePaths);
+                    string x = System.IO.Path.GetDirectoryName(folder);
+                    File.Exists(x);
+                    string ne_model = getBetween(folder, "Nokia_", "_R4");
+                    label8.Text = ne_model;
+                    string ne_release = getBetween(folder, "", "");
+                    label10.Text = ne_release;
+                    string adapter_version_communicator = getBetween(folder, "MGW_", ".dll");
+                    label7.Text = adapter_version_communicator;
+                    string Document_Name_Version = getBetween(folder, "", ".dll");
+                    label13.Text = Document_Name_Version;
+                    string md5 = CreateMD5(folder);
+                    label9.Text = md5;
+
+                }
+
+                if (DosyaUzantisi == ".nak")
+                {
+
+                    string[] filePaths = Directory.GetFiles(@directory, "*.docx");
+                    string wordfile = string.Join("", filePaths);
+                    string x = System.IO.Path.GetDirectoryName(folder);
+                    File.Exists(x);
+                    string ne_model = getBetween(folder, "nokia-", "-usp");
+                    label8.Text = ne_model;
+                    string ne_release = getBetween(folder, "", "");
+                    label10.Text = ne_release;
+                    string adapter_version_communicator = getBetween(folder, "usp1707_", ".nak");
+                    label7.Text = adapter_version_communicator;
+                    string Document_Name_Version = getBetween(folder, "", ".nak");
+                    label13.Text = Document_Name_Version;
+                    string md5 = CreateMD5(folder);
+                    label9.Text = md5;
+
+                }
+                if (DosyaUzantisi == ".jar")
+                {
+                   
+
+                    
+                    string[] filePaths = Directory.GetFiles(@directory, "*.docx");
+                    string wordfile = string.Join("", filePaths);
+                    string x = System.IO.Path.GetDirectoryName(folder);
+                    File.Exists(x);
+                    string ne_model = getBetween(folder, "NOKIA_", "_R31");
+                    label8.Text = ne_model;
+                    string ne_release = getBetween(folder, "", "");
+                    label10.Text = ne_release;
+                    string adapter_version_communicator = getBetween(folder, "DROUTER_", ".jar");
+                    label7.Text = adapter_version_communicator;
+                    string Document_Name_Version = getBetween(folder, "", ".jar");
+                    label13.Text = Document_Name_Version;
+                    string md5 = CreateMD5(folder);
+                    label9.Text = md5;
+                }
+
+
+            }
+
+
+
+
+        }
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+    
+
+
+
+
+
+
+        public void label8_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
